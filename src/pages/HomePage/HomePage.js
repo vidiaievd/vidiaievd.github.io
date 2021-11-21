e6c560ac699b5b34d28460541cc132fe536af962
VidiaievD <vidiaievdmytro@gmail.com>
2021-11-22 00:57:12 +0200

    added link in HomePage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom';
+
 export const HomePage = () => {
 	return (
 		<div>
-			<span>HomePage</span>
+			<p>HomePage</p>
+			<Link to="/employees">Employees Page</Link>
 		</div>
 	)
 }
